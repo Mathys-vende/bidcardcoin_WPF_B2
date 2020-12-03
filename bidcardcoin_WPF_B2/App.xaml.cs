a6681becb5e86566636a9e14d95e0c3227de8361
Mathys <Mathys.vende44@gmail.com>
2020-12-03 16:30:23 +0100

    First Push

---
+++ bidcardcoin_WPF_B2/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace bidcardcoin_WPF_B2
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App
+  {
+  }
+}
---
+++ bidcardcoin_WPF_B2/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace bidcardcoin_WPF_B2
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+  }
+}
