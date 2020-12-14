1d03edab3f848d99bf8da463048ab4324ba5449c
Mathys <Mathys.vende44@gmail.com>
2020-12-14 17:51:38 +0100

    Ajout Produit by tim

--- bidcardcoin_WPF_B2/Vue/Listes/listeProduits.xaml.cs
+++ bidcardcoin_WPF_B2/Vue/Listes/listeProduits.xaml.cs
@@ -1,15 +1,46 @@
+using bidcardcoin_WPF_B2.CTRL;
+using bidcardcoin_WPF_B2.ORM;
+
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using bidcardcoin_WPF_B2.DAL;
 
 
 namespace bidcardcoin_WPF_B2.Vue
 {
     public partial class listeProduits : UserControl
     {
+        private int selectedProduitId;
+        ProduitViewModel myDataObject;
+        ObservableCollection<ProduitViewModel> lp;
+        int compteur = 0;
         public listeProduits()
         {
             InitializeComponent();
 
+            DALConnection.OpenConnection();
+
+            loadProduits();
+
+        }
+
+        private void loadProduits()
+        {
+            lp = ProduitORM.listeProduit();
+            myDataObject = new ProduitViewModel();
+            //LIEN AVEC la VIEW
+            listeProduit.ItemsSource = lp; // bind de la liste avec la source, permettant le binding.
+        }
+        
+        private void listeProduit_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if ((listeProduit.SelectedIndex < lp.Count) && (listeProduit.SelectedIndex >= 0))
+            {
+                selectedProduitId = (lp.ElementAt<ProduitViewModel>(listeProduit.SelectedIndex)).idProduitProperty;
+            }
         }
 
         private void ajouterProduit(object sender, RoutedEventArgs e)
--- bidcardcoin_WPF_B2/Vue/ajouter/ajouterProduit.xaml.cs
+++ bidcardcoin_WPF_B2/Vue/ajouter/ajouterProduit.xaml.cs
@@ -1,13 +1,35 @@
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
+using bidcardcoin_WPF_B2.CTRL;
+using bidcardcoin_WPF_B2.DAL;
 
 namespace bidcardcoin_WPF_B2.Vue
 {
     public partial class ajouterProduit : UserControl
     {
+        private ProduitViewModel myDataObjectProduit;
+        public ObservableCollection<ProduitViewModel> Personne;
+        int compteur = 0;
+        
         public ajouterProduit()
         {
             InitializeComponent();
+            
+            DALConnection.OpenConnection();
+            
+            /*
+            loadProduit();
+
+            loadLot();
+
+            loadVendeur();
+
+            loadAcheteur();
+            
+            appliquerContexte();
+            */
+            
         }
 
 
