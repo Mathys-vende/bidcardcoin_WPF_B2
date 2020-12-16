58afaa126127c366fd8c269365716a324ed27976
TimDel <tim.del44210@gmail.com>
2020-12-16 15:08:33 +0100

    kyk

---
+++ bidcardcoin_WPF_B2/Vue/MainWindow.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using bidcardcoin_WPF_B2.ORM;
+using bidcardcoin_WPF_B2.Vue;
+
+namespace bidcardcoin_WPF_B2
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow
+  {
+
+
+    public MainWindow()
+    {
+      InitializeComponent();
+      
+    }
+    private void btnSubmit_Click(object sender, RoutedEventArgs e)
+    {
+            string email = txtUsername.Text;
+            string mdp = txtPassword.Password;
+
+            //email.Replace("@", ".");
+
+            if (PersonneORM.getAuth(email, mdp) == 1) 
+            { 
+            //todo: condition d'authentification
+                adminBaseWindow admin = new adminBaseWindow();
+                this.Close();
+                admin.Show(); 
+            }
+            else
+            {
+                MessageBox.Show("Email et/ou mot de passe incorrect", email);
+            }
+    }
+
+
+    }
+}
